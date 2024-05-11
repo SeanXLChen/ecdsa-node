ef5dc04fe7ec6b1bcafdc994686ba746cb965885
SeanXLChen <xiaolaiseanchen@gmail.com>
2024-05-11 15:56:09 -0700

    Feat: wallet use public key as address

--- client/src/Wallet.jsx
+++ client/src/Wallet.jsx
@@ -1,4 +1,8 @@
 import server from "./server";
+import { keccak256 } from "ethereum-cryptography/keccak";
+import { toHex, utf8ToBytes } from "ethereum-cryptography/utils";
+
+
 
 function Wallet({ address, setAddress, balance, setBalance }) {
   async function onChange(evt) {
@@ -19,8 +23,8 @@ function Wallet({ address, setAddress, balance, setBalance }) {
       <h1>Your Wallet</h1>
 
       <label>
-        Wallet Address
-        <input placeholder="Type an address, for example: 0x1" value={address} onChange={onChange}></input>
+        PublicKey
+        <input placeholder="Input compressed PublicKey" value={address} onChange={onChange}></input>
       </label>
 
       <div className="balance">Balance: {balance}</div>
